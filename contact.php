d8a882a45f96e8885a522fff3d2839175aa87c68
Peter Felis <Peter@felis.nl>
2023-04-07 15:54:27 +0200

    start

---
+++ contact.php
@@ -0,0 +1,58 @@
+<?php
+$title = 'contact';
+include_once('header.php'); ?>
+
+<body class="info">
+    <div class='section grid2col'>
+        <div class='gridcenter tc'>
+            <img class='img-back' src="afbeeldingen/hp-136.jpg" alt='muisje' />
+            <h1>contact info</h1>
+            <h3>Altijd handig</h3>
+        </div>
+
+
+        <div class="grid2col phl">
+            <div class="tr">Naam</div>
+            <div>Fetum</div>
+            <div class="tr">Adres</div>
+            <div>Grote waard 36</div>
+            <div class="tr">Postcode</div>
+            <div>2675 BX</div>
+            <div class="tr">Plaats</div>
+            <div>Honselersdijk</div>
+            <div class="tr">Telefoon</div>
+            <div>+31 (0) 174 769132</div>
+            <div class="tr">Mail</div>
+            <div>info@fetum.nl</div>
+            <div class="tr">KVK</div>
+            <div>Den Haag 28045481</div>
+            <div class="tr">BTW nummer</div>
+            <div>801462.265.B01</div>
+            <div class="tr">Bank</div>
+            <div>NL78 KNAB 0724 8909 47</div>
+            <div class="tr">BIC</div>
+            <div>KNABNL2H</div>
+        </div>
+
+    </div>
+
+
+    <div class="section fullwidth">
+        <iframe class="fullwidth" title="fetum" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2455.8254131692074!2d4.221463816410517!3d52.010059179720905!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5dac3b3643789%3A0x490ecab243450195!2sFetum!5e0!3m2!1sen!2snl!4v1655135966847!5m2!1sen!2snl" width="700" height="650" style="border:0;" allowfullscreen="true" loading="lazy" referrerpolicy="no-referrer-when-downgrade" />
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <?php include_once('footer.php'); ?>
